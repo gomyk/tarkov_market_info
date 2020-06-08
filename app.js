e3cf25cee48538b92860445b5e596ba1b7907469
Moonwon Kim <gomyk@naver.com>
2020-06-08 11:36:03 +0900

    Update app.js

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ var app = express();
 var db = mongoose.connection;
 
 db.on('error', console.error);
-db.once('open', function () {
-  console.log("Connected to mongo server");
+db.once('open', function() {
+    console.log("Connected to mongo server");
 });
 
 mongoose.connect('mongodb://localhost:27018/logging');
@@ -42,52 +42,62 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+app.use(function(req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error.html');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error.html');
 });
 
 const options = {
-  uri: "https://tarkov-market.com/api/v1/items/all",
-  headers: {
-    'x-api-key': 'eluBm1haTR4aYvXY'
-  }
+    uri: "https://tarkov-market.com/api/v1/items/all",
+    headers: {
+        'x-api-key': 'eluBm1haTR4aYvXY'
+    }
 };
 var time = null;
 startLogging();
 interval = setInterval(() => {
-  startLogging();
+    startLogging();
 }, 1000 * 60 * 60 * 2); //2 hours
 
+var download = function(uri, filename, callback) {
+    request.head(uri, function(err, res, body) {
+        console.log('content-type:', res.headers['content-type']);
+        console.log('content-length:', res.headers['content-length']);
+
+        request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+    });
+};
 
 function startLogging() {
-  time = Date.now();
-  console.log(time + ' : start Update DB');
+    time = Date.now();
+    console.log(time + ' : start Update DB');
 
-  request(options, function (err, response, body) {
-    var obj = JSON.parse(body);
-    console.log(obj.length);
-    obj.forEach(doc => {
-      doc.timestamp = time;
-      var price = new Price(doc);
-
-      price.save(function (err, object) {
-        if (err) {
-          return console.log(err);
-        }
-        console.log('[' + time + '] item name: ' + object.name + ' , logging success');
-      });
-    });
-  })
+    request(options, function(err, response, body) {
+        var obj = JSON.parse(body);
+        console.log(obj.length);
+        obj.forEach(doc => {
+            doc.timestamp = time;
+            var price = new Price(doc);
+            download(doc.icon, doc.name + '.png', function() {
+                console.log(doc.name + ' done..');
+            });
+            price.save(function(err, object) {
+                if (err) {
+                    return console.log(err);
+                }
+                console.log('[' + time + '] item name: ' + object.name + ' , logging success');
+            });
+        });
+    })
 }
 module.exports = app;
