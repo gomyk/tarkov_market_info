b77b70776b14f393e4b3d7cfef1b020d364a778c
Moonwon Kim <gomyk@naver.com>
2020-06-08 11:38:41 +0900

    Update app.js

--- app.js
+++ app.js
@@ -71,9 +71,6 @@ interval = setInterval(() => {
 
 var download = function(uri, filename, callback) {
     request.head(uri, function(err, res, body) {
-        console.log('content-type:', res.headers['content-type']);
-        console.log('content-length:', res.headers['content-length']);
-
         request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
     });
 };
