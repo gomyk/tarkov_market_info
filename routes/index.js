3a0837475fb217c20c75ced4ade992a04349b330
Moonwon Kim <gomyk@naver.com>
2020-06-08 12:19:39 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -29,13 +29,27 @@ router.get('/db', function (req, res, next) {
 });
 
 router.get('/db/latest', function(req, res, next) {
-    Price.find().distinct(function(err, doc) {
+    Price.find({}, {
+        skip: 0,
+        limit: 2000,
+        sort: {
+            timestamp: -1
+        }
+    }, function(err, doc) {
         if (err) {
             console.log(err);
             res.send(500, err);
             return;
         }
-        res.send(200, doc);
+        var mp = new Map();
+        var new_doc = [];
+        doc.forEach(d => {
+            if (!mp.has(d.name)) {
+                mp.set(d.name, true);
+                new_doc.push(d);
+            }
+        });
+        res.send(200, new_doc);
     });
 });
 module.exports = router;
